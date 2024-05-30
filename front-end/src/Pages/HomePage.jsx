cbc2a9ba9426ea231b1b22963508657f81d6a372
Jitu gandhare <jitugandhare@gmail.com>
2024-05-30 18:26:34 +0530

    some part of ui is done

--- front-end/src/Pages/HomePage.jsx
+++ front-end/src/Pages/HomePage.jsx
@@ -2,20 +2,40 @@ import React, { useEffect, useState } from 'react'
 import Banner from '../components/Banner';
 
 const HomePage = () => {
-  const [selectCategory,setSelectCategory]=useState(null);
-  const [jobs,setJobs]=useState([])
+  const [selectCategory, setSelectCategory] = useState(null);
+  const [jobs, setJobs] = useState([])
   const [query, setQuery] = useState("");
 
-  useEffect(()=>{
-    fetch("jobs.json").then(res=>res.json()).then(data=>{
-      console.log(data)
+  useEffect(() => {
+    fetch("jobs.json").then(res => res.json()).then(data => {
+      // console.log(data)
+      setJobs(data)
     })
-  },[])
+  }, [])
 
   const handleInputChange = (e) => {
     setQuery(e.target.value)
-    console.log(e.target.value)
+
   }
+
+  // filter jobs by title
+
+  const filterItems = jobs.filter((job) => job.jobTitle.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+  // console.log(filterItems)
+
+  // radio based button filter
+  const handleChange = (e) => {
+    setSelectCategory(e.target.value)
+  }
+
+
+  // buttn based filtering
+  const handleClick = (e) => {
+    setSelectCategory(e.target.value)
+  }
+
+
+
   return (
     <div>
       <Banner query={query} handleInputChange={handleInputChange} />
